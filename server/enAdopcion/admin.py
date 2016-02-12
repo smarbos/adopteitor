4d5f726f8e0c45827792c302ae92e519a9b61403
smarbos <smarbos@gmail.com>
2016-02-12 16:20:04 -0300

    Renamed enAdopcion as Animal

--- server/enAdopcion/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from enAdopcion.models import enAdopcion
-from enAdopcion.models import enAdopcionFotos
-admin.site.register(enAdopcion)
-admin.site.register(enAdopcionFotos)
